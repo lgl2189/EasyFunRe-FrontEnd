7feaf46312c15e7f2acb40c7193263b3c7d3710d
lgl2189 <1214525891@qq.com>
2026-03-21 22:46:05 +0800

    路由更新，添加登录等路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,45 @@
 import { createRouter } from 'vue-router'
 import { createWebHistory } from 'vue-router'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'HomeView',
+    component: () => import('@/views/HomeView.vue'),
+    alias: ['/home'],
+    children: [],
+  },
+  {
+    path: '/user',
+    children: [
+      {
+        path: 'center',
+        name: 'UserCenterView',
+        component: () => import('@/views/user/UserCenterView.vue'),
+      },
+      {
+        path: 'space',
+        // name: 'UserSpaceView',
+        // component: () => import('@/views/user/UserSpaceView.vue'),
+      },
+      {
+        path: 'login',
+        name: 'UserLoginView',
+        component: () => import('@/views/user/UserLoginView.vue'),
+      },
+    ],
+  },
+  {
+    path: '/error',
+    children: [
+      {
+        path: '404',
+        name: 'Error404View',
+        component: () => import('@/views/error/Error404View.vue'),
+      },
+    ],
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
